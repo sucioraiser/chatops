5137ccb4156f8bde45b9b5088900d417c504be92
Pat Ma Roo <pa.mardones@gmail.com>
2020-02-28 00:02:42 -0300

    Update server.js

--- server.js
+++ server.js
@@ -9,17 +9,18 @@ app.use(bodyParser.json());
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 
-
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 
 // Again, we define a port we want to listen to
 //Cambiar segun puerto de conexion
-const PORT=8880;
+//const port=8880;
 
 // Lets start our server
-app.listen(PORT, function () {
+app.listen(port, function () {
    //Callback triggered when server is successfully listening. Hurray!
-    console.log("Inicializando App:  " + PORT);
+    console.log("Inicializando App:  " + port);
    console.log("Estable:");
 });
 
